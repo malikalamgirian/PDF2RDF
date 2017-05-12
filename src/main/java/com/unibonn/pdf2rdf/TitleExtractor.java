26cb138567dea71b31e633a013946ef6a897be8e
Wasif Altaf <malikalamgirian@gmail.com>
2017-05-12 23:41:58 +0200

    Basic Title Extractor

--- src/main/java/com/unibonn/pdf2rdf/TitleExtractor.java
+++ src/main/java/com/unibonn/pdf2rdf/TitleExtractor.java
@@ -1,19 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.unibonn.pdf2rdf;
 
 import java.io.ByteArrayOutputStream;
@@ -31,7 +15,7 @@ import org.apache.pdfbox.text.TextPosition;
 /**
  * This class will extract title from the given PDF
  *
- * @author Wasif Altaf, Ben Litchfield
+ * @author Wasif Altaf, Asif Altaf, Ben Litchfield
  */
 public class TitleExtractor extends PDFTextStripper {
 
